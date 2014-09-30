5b9a90997ca43e57a6dec3003634fe591d95d7fb
dlatour <dlatour@apple.com>
2014-09-30 12:54:45 -0700

    Finished unit tests, completed all initial functionality. Updated README file with latest features.

--- README.md
+++ README.md
@@ -1,85 +1,81 @@
 #  logan
 
-*Provides advanced logging services for your modules. Allows for easy configuration using a JSON file, can log to the console and/or the file system, supports rotating logs files, allows for inclusion or exclusion of messages by module or level, provides four levels of messaging, configuration is by computer therefore each PC may have its own configuration.*
+*Provides advanced logging services for your modules. Allows for easy configuration using a JSON file, can log to the console and/or the file system, supports rotating logs files, allows for inclusion or exclusion of messages by module and/or level, provides customizable levels of messaging, configuration is by hostname, environment variable, or default therefore each PC may have its own configuration.*
 
 ## Features
+
 - Log to the console, the file system, or both.
-- Provides four levels of logging: debug, info, warn, and error.
-- Configration via JSON, loads the config file based on the current PC's host name.
-- Optionally If the LOGAN_CONFIG environment variable is set, it loads the config file based on that rather than the PC.
-- If there are no config files and no environment variable it defaults to full logging.
-- You can change environment variables on the fly in code using the API. Any time you make a change, Logan stores your changes to the current config file. Optionally, you can specify not to save changes.
-- You can optionally include or exclude module messages based on partial path matching and levels.
+- Provides customizable levels of logging, defaults to `debug`, `info`, `warn`, and `error`.
+- Configuration via JSON, attempts to load the config in the following order, it stops once one of the methods is successful:
+	- Check for a LOGAN_CONFIG environment variable, if it exists then load the config file indicated by the variable.
+	- Check the `package.json` version, if it includes `-alpha`, `beta`, or `-rc` then use the included `testing.json` config found in the application directory.
+	- Check for a configuration file named after the host, if it exists then load it.
+	- Check for the default `logan.json` config file in the application directory, if it exists load it.
+	- If no config files are found the application defaults to full logging.
+- You can optionally filter module messages based on partial path matching and levels matching (includeFilters, excludeFilters).
 - Messages can optionally include JSON metadata.
 - You can configure the number of log files to save while rotating.
-- The number of levels is customizable.
+
 ## Installation
+
 `npm install -g logan`
 
 **See [Environment Setup](https://interactive-git.apple.com/interactive-qa-fe/Environment-Setup) if you have trouble installing**
 
 ## Usage
+
 ### Programmatic Interface
+
 You begin by instantiating the module.
+
 ```javascript
-// The create method takes in the current module id.
-var log = require("logan").create(module.id);
+    // The create method takes in the current module id.
+    var log = require("logan").create(module.id);
 
-// Include all 'warn' and 'debug' messages from all modules within /qa-svn/
-log.addIncludeFilter("/qa-svn/", [Level.WARN, Level.DEBUG]);
-
-// Exclude all messages from all modules within /qa-svn/test/ but only for the file log.
-log.addExcludeFilter("/qa-svn/test/", [Level.DEBUG, Level.INFO, Level.WARN, Level.ERROR], Transport.FILE);
-
-// Set a minimum log level for a transport
-log.setMinimumLevel(Level.WARN, Transport.FILE);
-
-// Set a minimum log level for all transports
-log.setMinimumLevel(Level.WARN);
-
-// Basic usage
-log.debug("This is a debug message", {name: "David", age: 46});
-log.error("An error occurred");
+    // Basic usage
+    log.debug("This is a debug message", {name: "David", age: 46});
+    log.error("An error occurred");
 ```
+
 Example configuration file
+
 ```json
-{
-  "baseName": "mylog",
-  "transports": [
-  	{"name":"console", "colorize": true, "level": "debug", "handleExceptions": true, "silent": false},
-  	{"name":"file", "saveCount": 3, "level": "debug", "handleExceptions": true, "silent": false}
-  ],
-  "includeFilters": [
-    {"filter": "/qa-svn/", "levels": ["warn", "debug"]}
-  ],
-  "excludeFilters": [
-    {"filter": "/qa-svn/test/", "levels": ["warn", "debug", "error", "info"]}
-  ],
-  "timestamp": {"includeDate": true, "includeMilliseconds": true}
-  "showModule": true,
-  "colors": {
-    "debug": "green",
-    "info": "green",
-    "warn": "red",
-    "error": "red"
-  },
-  "levels": {
-  	"debug": 0,
-    "info": 1,
-    "warn": 2,
-    "error": 3
-  },
-  "exitOnError": false,
-}
+    {
+      "baseName": "mylog",
+      "transports": [
+        {"name":"console", "colorize": true, "level": "debug", "handleExceptions": true, "silent": false},
+        {"name":"file", "saveCount": 5, "level": "debug", "handleExceptions": true, "silent": false}
+      ],
+      "includeFilters": [
+        {"pattern": "*", "levels": ["debug", "info", "warn", "error"]}
+      ],
+      "timestampSettings": {"includeDate": true, "includeMilliseconds": true},
+      "showModule": true,
+      "colors": {
+        "debug": "green",
+        "info": "yellow",
+        "warn": "red",
+        "error": "red"
+      },
+      "levels": {
+        "debug": 0,
+        "info": 1,
+        "warn": 2,
+        "error": 3
+      },
+      "exitOnError": false
+    }
 ```
+
 #### Options
 
 *Results take the form of*
 
 ```shell
-2014-09-26 10:02:35.458 - debug: [/interactive-qa-fe/logan/lib/test.js] - Debugging with test
-2014-09-26 10:02:35.460 - debug: [/interactive-qa-fe/logan/lib/test.js] - Executing test
+    2014-09-26 10:02:35.458 - debug: [/interactive-qa-fe/logan/lib/test.js] - Debugging with test
+    2014-09-26 10:02:35.460 - debug: [/interactive-qa-fe/logan/lib/test.js] - Executing test
 ```
 
 ## License
+
 Copyright © 2014 Apple Inc. All rights reserved.
--- test/logan_tests.js
+++ test/logan_tests.js
@@ -1,22 +1,185 @@
 "use strict";
 
+var fs = require("fs-extra");
+var path = require("path");
 var os = require("os");
 
 var chai = require("chai");
 var expect = chai.expect;
 
 var Logan = require("../lib/logan");
-var log = Logan.create(module.id);
+var LoganProxy = require("../lib/logan_proxy");
+
 
 describe("Logan", function() {
 
-  describe("debug", function() {
+  describe("configuration", function() {
 
-    it("sends a message to the console", function(done) {
+    it("loads from an environment variable", function(done) {
+      process.env.LOGAN_CONFIG = "../test/fixtures/environment.json";
+      Logan.config = null;
+      Logan._loadConfig();
+      expect(Logan.config).to.be.an.instanceOf(Object);
+      expect(Logan.config.baseName).to.eql("environment");
+      done();
+    });
 
+    it("loads from a host file in the application directory", function(done) {
+      delete process.env.LOGAN_CONFIG;
+      Logan.config = null;
+      Logan._loadConfig();
+      expect(Logan.config).to.be.an.instanceOf(Object);
+      expect(Logan.config.baseName).to.eql("development");
+      done();
+    });
+
+    it("loads the default configuration", function(done) {
+      process.env.LOGAN_CONFIG = "../test/fixtures/missing.json";
+      var originalFunction = os.hostname;
+      os.hostname = function() {
+        return "missing.local";
+      };
+      Logan.config = null;
+      Logan._loadConfig();
+      expect(Logan.config).to.be.an.instanceOf(Object);
+      expect(Logan.config.baseName).to.eql("logan");
+      os.hostname = originalFunction;
       done();
 
     });
+
   });
+
+  describe("logging", function() {
+
+    var logFilePath = path.resolve(__dirname, "../development.log");
+
+    afterEach(function() {
+      fs.deleteSync(logFilePath);
+    });
+
+    it("creates a log file and logs four messages", function(done) {
+      var log = Logan.create(module.id);
+      log.debug("This is a 'debug' message.");
+      log.info("This is an 'info' message.");
+      log.warn("This is a 'warn' message.");
+      log.error("This is an 'error' message.");
+
+      var intervalObj = setInterval(onInterval, 1000);
+
+      function onInterval() {
+        if (fs.existsSync(logFilePath)) {
+          clearInterval(intervalObj);
+          fs.readFile(logFilePath, "utf8", function(error, data) {
+            expect(data).to.include("debug");
+            expect(data).to.include("info");
+            expect(data).to.include("warn");
+            expect(data).to.include("error");
+            done();
+          });
+        }
+      }
+
+    });
+
+  });
+
+  describe("logan proxy", function() {
+    it("creates a proxy", function(done) {
+      var log = Logan.create(module.id);
+      expect(log).to.be.an.instanceOf(LoganProxy);
+      expect(log.moduleId).to.eql("/interactive-qa-fe/logan/test/logan_tests.js");
+      expect(log.debug).to.be.instanceOf(Function);
+      expect(log.info).to.be.instanceOf(Function);
+      expect(log.warn).to.be.instanceOf(Function);
+      expect(log.error).to.be.instanceOf(Function);
+      done();
+    });
+  });
+
+  describe("local timestamp", function() {
+    it("creates a default timestamp", function(done) {
+      Logan.config.timestampSettings.includeDate = true;
+      Logan.config.timestampSettings.includeMilliseconds = true;
+      var timestamp = Logan.localTimestamp(new Date("8/31/1968 5:45 pm"));
+      expect(timestamp).to.eql("1968-08-31 17:45:00.000");
+      done();
+    });
+
+    it("creates timestamp without date", function(done) {
+      Logan.config.timestampSettings.includeDate = false;
+      Logan.config.timestampSettings.includeMilliseconds = true;
+      var timestamp = Logan.localTimestamp(new Date("8/31/1968 5:45 pm"));
+      expect(timestamp).to.eql("17:45:00.000");
+      done();
+    });
+
+    it("creates timestamp without milliseconds", function(done) {
+      Logan.config.timestampSettings.includeDate = true;
+      Logan.config.timestampSettings.includeMilliseconds = false;
+      var timestamp = Logan.localTimestamp(new Date("8/31/1968 5:45 pm"));
+      expect(timestamp).to.eql("1968-08-31 17:45:00");
+      done();
+    });
+
+    it("creates timestamp without date and milliseconds", function(done) {
+      Logan.config.timestampSettings.includeDate = false;
+      Logan.config.timestampSettings.includeMilliseconds = false;
+      var timestamp = Logan.localTimestamp(new Date("8/31/1968 5:45 pm"));
+      expect(timestamp).to.eql("17:45:00");
+      done();
+    });
+
+  });
+
+  describe("message filtering", function() {
+
+    beforeEach(function() {
+      Logan.config.includeFilters = [];
+      Logan.config.excludeFilters = [];
+    });
+
+    it("prevents all modules from messaging by default", function(done) {
+      var isAllowed = Logan.isAllowed(module.id, "debug");
+      expect(isAllowed).to.eql(false);
+        done();
+    });
+
+    it("allows all modules to message", function(done) {
+      Logan.config.includeFilters.push({pattern:"*", levels: ["debug", "info", "warn", "error"]});
+      var isAllowed = Logan.isAllowed(module.id, "debug");
+      expect(isAllowed).to.eql(true);
+      done();
+    });
+
+    it("allows no modules with '/test/' in the path to message", function(done) {
+      Logan.config.includeFilters.push({pattern:"*", levels: ["debug", "info", "warn", "error"]});
+      Logan.config.excludeFilters.push({pattern:"/test/", levels: ["debug", "info", "warn", "error"]});
+      var isAllowed = Logan.isAllowed(module.id, "debug");
+      expect(isAllowed).to.eql(false);
+      done();
+    });
+
+    it("allows an excluded module to message with the correct level", function(done) {
+      Logan.config.includeFilters.push({pattern:"*", levels: ["debug", "info", "warn", "error"]});
+      Logan.config.excludeFilters.push({pattern:"/test/", levels: ["debug", "info"]});
+      var isAllowed = Logan.isAllowed(module.id, "debug");
+      expect(isAllowed).to.eql(false);
+      isAllowed = Logan.isAllowed(module.id, "error");
+      expect(isAllowed).to.eql(true);
+      done();
+    });
+
+    it("allows an included module to message with the correct level", function(done) {
+      Logan.config.includeFilters.push({pattern:"*", levels: ["warn", "error"]});
+      var isAllowed = Logan.isAllowed(module.id, "debug");
+      expect(isAllowed).to.eql(false);
+      isAllowed = Logan.isAllowed(module.id, "error");
+      expect(isAllowed).to.eql(true);
+      done();
+    });
+
+  });
+
 
 });
